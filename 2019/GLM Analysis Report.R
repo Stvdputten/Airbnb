fb791142feb4063e7fe14274417f66366ce03a7a
Deniz Sen <denizsen.edu@gmail.com>
2019-12-13 20:43:45 +0100

    Linear Regression Assumption Diagnostics were added: 1. non-constant error variance 2. Normality of errors 3. Linearity: Y is a linear function of X

--- 2019/GLM Analysis Report.R
+++ 2019/GLM Analysis Report.R
@@ -1,5 +1,7 @@
 ####----Libraries
 library(dplyr)
+library(ggplot2)
+library(car)
 #########---------- Linear Models AIR BNB Analysis -------------#########
 
 
@@ -96,22 +98,125 @@ sum(is.na(bnb$square_feet)) # 19873 # high missing values
 # Outcome variable 
 # price[,61](weeky_price[,62], monthly_price[,63])
 
-
 #########---------- Data Preperation & Examination -------------#########
 
 #----------create select subset of data & prepare for analysis 
 
 subs = as.data.frame(bnb[c(23:24,26,32,36:40,42:48,52:58,64:66,83:84,87:93,
                            99,106,61)])
+# some preprocessing done in Precessing R file
+subset = read.csv('2019/subset.csv')
 
-# examine type of each column
+colnames(subset) # variables review scores can be removed except review score rating
+# and number of review scores review score 
+colnames(subset[,19]) # review scorerating
+colnames(subset[,17]) # number_of_reviews
 
-i = 1
-for (i in i:38){
-print(typeof(subs[,i]))
-i = i + 1
+# remove additonal variables
+subset = subset[-c(1,3,18,20:25)]
 
-}
-# recode variables for analysis 
-subs[,38]= as.numeric(gsub("\\$", "", subs[,38]))
+# some NA can be recoded into 0 e.g. security deposite & cancelation fee
+subset$security_deposit[is.na(subset$security_deposit)] = 0 
+subset$cleaning_fee[is.na(subset$cleaning_fee)] = 0 
+
+# remove NA rows 
+subset = na.omit(subset)
+# number of variables in dataset
+ncol(subset)
+colnames(subset)
+
+#########---------- Regression Assumptions -------------#########
+x = model.matrix(price ~ ., subset)
+l_m = lm(price ~ x, subset)
+summary(l_m)
+#----------------- Nomal Errors ------------------#
+l_m_res = as.data.frame(l_m$residuals)
+
+ggplot(l_m_res, aes(sample = l_m_res$`l_m$residuals`)) + stat_qq() +
+  ggtitle("Quantile comparison plot Errors") + theme_light() +
+  labs(x= "Theoretical Quantiles", y= "Error Quantiles") + stat_qq_line()
+
+plot(density(l_m_res$`l_m$residuals`))
+
+#----------------- Constant Errors ------------------#
+
+plot(subset$price,l_m_res$`l_m$residuals`, main="Residual plot")
+
+ggplot(subset,aes(subset$price,
+                 l_m_res$`l_m$residuals`)) +
+  geom_point() + theme_light() + labs(x= "Agression", y= "Residuals") +
+  geom_smooth(method= "loess", se=FALSE) + ggtitle("Residual plot")
+
+# Rule of Thumb for non-constant error variance
+# if ratio of smallest vs biggest residual is bigger than 10 or 4 its a problem
+abs(max(l_m$residuals)) / abs(min(l_m$residuals)) < 10
+abs(max(l_m$residuals)) / abs(min(l_m$residuals)) < 4
+
+
+#----------------- Linearity ------------------#
+lin_c = as.data.frame(subset[c(1:19)])
+colnames(lin_c)
+
+# Save variables sepeartely for Component + Residual plot
+host_response_time = lin_c$host_response_time
+host_has_profile_pic = lin_c$host_has_profile_pic
+host_identity_verified = lin_c$host_identity_verified
+neighbourhood_cleansed = lin_c$neighbourhood_cleansed
+property_type = lin_c$property_type
+room_type = lin_c$room_type
+accommodates = lin_c$accommodates
+bathrooms = lin_c$bathrooms
+bedrooms = lin_c$bedrooms
+beds = lin_c$beds 
+bed_type = lin_c$bed_type
+security_deposit = lin_c$security_deposit
+cleaning_fee = lin_c$cleaning_fee
+guests_included = lin_c$guests_included
+number_of_reviews = lin_c$number_of_reviews
+review_scores_rating = lin_c$review_scores_rating
+cancellation_policy = lin_c$cancellation_policy
+reviews_per_month = as.integer(lin_c$reviews_per_month)
+
+# same lm model as before, but lets make sure its here
+l_m = lm(price ~ ., lin_c)
+
+
+# Component + Residual plot to examine the relationship between Y and each X 
+# individually. CoPlots show the  partial relationship of Y and X 
+# controlling for other Xs.
+crPlot(l_m, host_response_time)
+crPlot(l_m, host_has_profile_pic)
+crPlot(l_m, host_identity_verified)
+crPlot(l_m, neighbourhood_cleansed)
+crPlot(l_m, property_type)
+crPlot(l_m, room_type)
+crPlot(l_m, accommodates)
+crPlot(l_m, bathrooms)
+crPlot(l_m, beds)
+crPlot(l_m, bed_type)
+crPlot(l_m, security_deposit)
+crPlot(l_m, cleaning_fee)
+crPlot(l_m, guests_included)
+crPlot(l_m, number_of_reviews)
+crPlot(l_m, cleaning_fee)
+crPlot(l_m, review_scores_rating)
+crPlot(l_m, cancellation_policy)
+crPlot(l_m, reviews_per_month)
+
+
+#----------------- Influential Cases ------------------#
+
+#---------- Hat-Values 
+hat_v = hatvalues(l_m)
+#---------- studentized Residuals
+s_resid = rstudent(l_m)
+#---------- Cook's Residuals --------#
+cd = as.vector(cooks.distance(l_m))
+cooks.distance(l_m)
+
+# bubble plot of laverage vs discrepancy 
+ggplot(l_m, aes(x = hat_v, y = s_resid, size = cd)) +
+  geom_point(aes(color = )) + theme_light() +
+  ggtitle("Leverage vs. Studentized residuals") +
+  labs(x = "Leverage", y = "Studentized Residuals", size = "Cook's Distance")
 
